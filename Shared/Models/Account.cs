745e66c5eeafffe26c3e7585085b7a434250004e
kevorkkeheian <kevork.keheian@codgram.com>
2021-02-11 13:27:49 +0200

    [Clean] remove unecessary using statements

--- Shared/Models/Account.cs
+++ Shared/Models/Account.cs
@@ -1,10 +1,6 @@
 using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Identity;
 using Profile.Shared.Models.Admin;
 
 namespace Profile.Shared.Models
