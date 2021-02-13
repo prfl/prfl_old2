65c5ab89e827b7b996e5b37555e606cdc661e96c
kevorkkeheian <kevork.keheian@codgram.com>
2021-02-13 12:47:48 +0200

    [Feature] add recipe controller

---
+++ Server/Controllers/RecipeController.cs
@@ -0,0 +1,121 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Profile.Server.Data;
+using Profile.Shared.Models;
+
+namespace Profile.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RecipeController : ControllerBase
+    {
+        private readonly ProfileContext _context;
+        private readonly UserManager<ProfileUser> _userManager;
+
+        public RecipeController(ProfileContext context, UserManager<ProfileUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: api/Recipe
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetRecipe()
+        {
+            return await _context.Recipe.ToListAsync();
+        }
+
+        // GET: api/recipe/u/{username}
+        [HttpGet("u/{username}")]
+        [AllowAnonymous]
+        public async Task<ActionResult<IEnumerable<Recipe>>> GetLinkByUsername(string username)
+        {
+            var user = await _userManager.FindByNameAsync(username);
+            return await _context.Recipe.Where(l => l.ProfileUserId == user.Id).ToListAsync();
+        }
+
+        // GET: api/Recipe/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Recipe>> GetRecipe(string id)
+        {
+            var recipe = await _context.Recipe.FindAsync(id);
+
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            return recipe;
+        }
+
+        // PUT: api/Recipe/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutRecipe(string id, Recipe recipe)
+        {
+            if (id != recipe.RecipeId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(recipe).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!RecipeExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Recipe
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Recipe>> PostRecipe(Recipe recipe)
+        {
+            _context.Recipe.Add(recipe);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetRecipe", new { id = recipe.RecipeId }, recipe);
+        }
+
+        // DELETE: api/Recipe/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteRecipe(string id)
+        {
+            var recipe = await _context.Recipe.FindAsync(id);
+            if (recipe == null)
+            {
+                return NotFound();
+            }
+
+            _context.Recipe.Remove(recipe);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool RecipeExists(string id)
+        {
+            return _context.Recipe.Any(e => e.RecipeId == id);
+        }
+    }
+}
