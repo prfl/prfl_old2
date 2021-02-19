9e9144bff1b68b9567d92df4fdf73c2500e23254
kevorkkeheian <kevork.keheian@codgram.com>
2021-02-19 14:35:21 +0200

    [Update] add product details

---
+++ Server/Pages/Products/Details.cshtml.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Octokit;
+using Profile.Server.Data;
+using Profile.Shared.Models;
+
+namespace Profile.Server.Pages.Products
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly ProfileContext _context;
+
+        public DetailsModel(ProfileContext context)
+        {
+            _context = context;
+        }
+        
+        public Product Product { get; set; }
+        public async Task<IActionResult> OnGetAsync(string productId)
+        {
+            Product = await _context.Product.FirstOrDefaultAsync(p => p.ProductId == productId);
+            return Page();
+        }
+    }
+}
