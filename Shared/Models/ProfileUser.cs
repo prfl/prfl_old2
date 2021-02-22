d7332a866f150b61ff5e9f96c5e45b4f90416b51
kevorkkeheian <kevork.keheian@codgram.com>
2021-02-22 15:33:26 +0200

    [Feature] add ImageUrl

--- Shared/Models/ProfileUser.cs
+++ Shared/Models/ProfileUser.cs
@@ -27,5 +27,6 @@ namespace Profile.Shared.Models
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public ProfileUserType ProfileUserType { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
